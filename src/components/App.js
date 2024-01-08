38f0dad8a992f061c2aa50453b1930511129cffc
Dakota Vikdal <dakota.james19@hotmail.com>
2024-01-07 21:11:11 -0500

    redoing lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,11 @@ function Home() {
 }
 
 function About() {
-  return <div id = 'about'>
-    <h2>About</h2>
-  </div>  
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
 }
 
 
